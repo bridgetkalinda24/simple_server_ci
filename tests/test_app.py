241b653fc44e04b0bc0da6cafc21c09d436cb1fc
Bridget Kalinda <bridgetkalinda@yahoo.co.uk>
2025-05-12 14:20:37 +0100

    included the sentences on new lines

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world! This is my first deployment and I am excited to keep learning! I am a CI-CD hero!"
+    assert response.data.decode("utf-8") == "Hello, world! /nThis is my first deployment and /nI am excited to keep learning! /nI am a CI-CD hero!"
